a54c2c9d4b13a46488b2fa798d3a184d6523e84d
szymsad <szymsadzik@gmail.com>
2026-04-21 23:50:24 +0200

    Rodział 6 - Projekt MAUI

---
+++ CsvMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CsvMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CsvMobile/CsvPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace CsvMobile;
+
+public partial class CsvPage : ContentPage
+{
+    public CsvPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void WczytajPlik(object sender, EventArgs e)
+    {
+        try
+        {
+            var wynik = await FilePicker.PickAsync(new PickOptions
+            {
+                PickerTitle = "Wybierz plik CSV",
+                FileTypes = new FilePickerFileType(
+                    new Dictionary<DevicePlatform, IEnumerable<string>>
+                    {
+                    { DevicePlatform.Android, new[] { "text/csv", "text/comma-separated-values" } }
+                    })
+            });
+
+            if (wynik == null) return;
+
+            statusLabel.Text = $"Wybrano: {wynik.FileName}";
+        }
+        catch (Exception ex)
+        {
+            statusLabel.Text = $"Błąd: {ex.Message}";
+        }
+    }
+}
